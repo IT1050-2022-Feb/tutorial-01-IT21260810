fae345e886764669f9922a44e39de188229c7bb3
IT21260810 <99312190+IT21260810@users.noreply.github.com>
2022-02-25 04:52:09 +0000

    Completed 03

--- Tute03.c
+++ Tute03.c
@@ -1,34 +1,18 @@
-#include<stdio.h>
-float area(float length, float width);
-float Harea(float Hlength, float Hwidth);
-int main()
-{
-	float width,length,Hwidth,Hlength,House=0,Yard=0,Lawn=0;
-	
-	printf("\nEnter the length of the yard : ");
-	scanf("%f",&length);
-	
-	printf("\nEnter the width of the yard : ");
-	scanf("%f",&width);
-	
-		
-	printf("\nEnter the width of the house : ");
-	scanf("%f",&Hwidth);
-	
-		
-	printf("\nEnter the length of the house : ");
-	scanf("%f",&Hlength);
+#include <stdio.h>
+int main() {
 
-	
-	
-	printf("\nArea of the Yard : %.2f",area(length,width)); 
-	printf("\nArea of the House : %.2f",area(Hlength,Hwidth)); 
-	printf("\nArea of the Lawn : %.2f",area(length,width)-area(Hlength,Hwidth)); 
-}
+  int num , i , sum = 0 ;
 
-float area(float length, float width)
-{
-	float area;
-	area = length*width;
-	return area;
+  printf("\nEnter the number : ");
+  scanf("%d" , &num);
+
+  for(i=1; i<=num; i++)
+  {
+    sum = sum +i ;
+
+  }
+
+  printf("\n%d" , sum);
+  
+  return 0;
 }
